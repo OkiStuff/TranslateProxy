c1f47ab58fc089d4f9ad732d8b2e036dcaefebea
OkiStuff <probro1802@gmail.com>
2021-10-13 17:12:15 -0400

    v2

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,38 @@
 <script>
 	let url = "";
+	let search = "";
+
 	let prefix = "https://translate.google.com/translate?sl=en&tl=en&u=";
+
+	let bing_redirect_mode;
+	let prefix_bing = "https://bing.com/search?q=";
+
+	let blocked_urls = [
+		"https://www.buzzfeed.com",
+		"https://buzzfeed.com",
+		"http://www.buzzfeed.com",
+		"http://buzzfeed.com"
+	];
 	let redirect_mode;
 
 	const handleSubmit = () => {
+		//blocked_urls.forEach(element => {
+		//	if (url == element) alert("no");
+		//});
+
 		if (redirect_mode == 0) window.location.replace(`${prefix}${url}`); // Simulate HTTP redirect
 		if (redirect_mode == 1) window.location.href = `${prefix}${url}`;
 	};
 
+	const handleSearch = () => {
+		if (redirect_mode == 0) window.location.replace(`${prefix}${prefix_bing}${search}`); // Simulate HTTP redirect
+		if (redirect_mode == 1) window.location.href = `${prefix}${prefix_bing}${search}`;
+	}
+
 </script>
 
 <main>
-	<h1>TranslateProxy</h1>
+	<h1>TranslateProxy v2</h1>
 	<p>Use Google Translate to bypass website blocking</p>
 
 	<form on:submit|preventDefault = {handleSubmit}>
@@ -26,8 +47,21 @@
 
 	</form>
 
+	<p>or</p>
+
+	<form on:submit|preventDefault = {handleSearch}>
+		<input bind:value={search} placeholder="Search on Bing">
+		<select bind:value={bing_redirect_mode}>
+			<option value=1>JS Redirect</option>
+			<option value=0>HTTP Redirect</option>
+		</select>
+
+		<button>Submit</button>
+
+	</form>
+
 	<footer>
-		<p>&copy; OkiStuff - 2021, All Rights Reserved. <br /><a href="https://mit-license.org/">View the license here</a></p>
+		<p>&copy; OkiStuff - 2021, All Rights Reserved. <br /><a href="https://mit-license.org/">View the license here</a><br /><a href="/learn_more.html">Learn More</a></p>
 	</footer>
 </main>
 <style>
